a05179215739b2a32b41825ffc498dd8ff61d31f
kenneth pegueros <kenneth_183@hotmail.com>
2023-05-01 16:35:22 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/cicloinfinitoej2/CicloinfinitoEj2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cicloinfinitoej2;
+
+/**
+ *
+ * @author kenne
+ */
+public class CicloinfinitoEj2 {
+
+    public static void main(String[] args) {
+        
+        for (;;){
+        System.out.println("¡Esto no acaba!");
+        }
+    }
+}
